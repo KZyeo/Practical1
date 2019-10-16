96d8220379536601d943c6f6decd6959854bfa61
Kz <yeokz-pm17@student.tarc.edu.my>
2019-10-16 10:03:38 +0800

    Initial commit

---
+++ app/src/main/java/com/example/practical1/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.practical1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val rollButton: button = findViewById(R.id.roll_button)
+    }
+}
